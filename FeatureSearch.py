703da2402d08fbc39d9bc2403b27fb66a0db62e1
ccalv013 <91503987+ccalv013@users.noreply.github.com>
2024-06-08 19:45:17 -0700

    fixing part 2 and add 3

--- FeatureSearch.py
+++ FeatureSearch.py
@@ -1,5 +1,10 @@
 import random 
 import copy
+import numpy as np
+from collections import Counter
+from operator import itemgetter
+import os
+import math
 
 class Node():
     def __init__(self, features=None, score=0):
@@ -31,54 +36,52 @@ class Node():
         return copy.deepcopy(self)
 
     def print_node(self):
-        print(f"\tUsing feature(s): {self.features} | Accuracy is: {self.score*100}%\n")
+        print(f"\tUsing feature(s): {self.features} | Accuracy is: {(self.score*100):.2f}%\n")
     
 def dummy_evaluation(features):
     return round(random.uniform(0.0, 1.0), 3)
 
-def BE(num_features):
-    #create an array 'individual scores' that holds 'num_features' amount of 'Node' instances, each with a feature subset of [1..num_features]
-    individual_scores = [Node([i for i in range(1, num_features+1)]) for _ in range(num_features)]
-    best_features = Node()
+def BE(data):
+    max_feature = data.shape[1] - 1
+    #individual_scores = [Node([i for i in range(1, max_feature)]) for _ in range(max_feature)]
+    individual_scores = []
+    best_features = Node(list(range(1, max_feature+1)))
 
-    #loop through every 'Node' contained within 'individual scores' and remove a unique feature from every subset, assigning every node a score
-    #initialize 'best_features' as a node containing the subset of features [1..num_features], i.e. all features included
-    for i in range(0, num_features):
-        individual_scores[i].remove_features(i)
-        individual_scores[i].set_score(dummy_evaluation(individual_scores[i].get_features()))
-        best_features.add_features(i+1)
+    best_features.set_score(leave_one_out_cross_validation(data, best_features.get_features()))
 
-    best_features.set_score(dummy_evaluation(best_features.get_features()))
-    individual_scores = sorted(individual_scores, key=lambda node: node.get_score())
+    #for i in range(1, max_feature):
+    #    individual_scores[i].remove_features(i + 1)
+    #    individual_scores[i].set_score(leave_one_out_cross_validation(data, individual_scores[i].get_features()))
+    #    best_features.add_features(i+1)
 
-    #generate default rate NOTE: I don't think we actually end up using default rate here, rather threshold --will followup
-    default_rate = dummy_evaluation(0)
-    print(f"\nUsing no features and 'random' evaluation, I get an accuarcy of {default_rate*100}%\n")
+    #best_features.set_score(leave_one_out_cross_validation(data, best_features.get_features()))
+
+    #default_rate = dummy_evaluation(0)
 
     print(f"Model accuracy considering ALL features: {best_features.get_score()*100}%\n")
     print(f"Beginning search.\n")
-    for i in range(0, num_features):
-        individual_scores[i].print_node()
+    for i in range(0, max_feature - 1):
+        temp_node = Node(list(np.delete(best_features.get_features(), i)))
+        temp_node.set_score(leave_one_out_cross_validation(data, temp_node.get_features()))
+        temp_node.print_node()
+        individual_scores.append(temp_node)
  
-    #first iteration happens outside loop
-    #LHS: maximum performing subset, minus one feature
-    #RHS: threshold to beat (must be 5% greater than best score thus far)
-    if individual_scores[num_features-1].get_score() >= 0.95 * best_features.get_score():
-        best_features = individual_scores[num_features-1]
+    individual_scores = sorted(individual_scores, key=lambda node: node.get_score())
+
+    if individual_scores[len(individual_scores) - 1].get_score() >= best_features.get_score():
+        best_features = individual_scores[len(individual_scores) - 1]
     else:
         print(f"(Warning accuracy has decreased)\n")
         return best_features
 
-    for _ in range(num_features):
-        #print the performance of the last subset
+    for _ in range(max_feature):
         print(f"Feature set: {best_features.get_features()} was best, accuracy is {best_features.get_score()*100}%\n")
         current_tree_level = []
 
-        #populate the 'level' of this tree with Node instances containing feature subsets of [best_features - `some` feature]
         for feature in range(0, len(best_features.get_features())):
             temp_node = best_features.copy_node()
             temp_node.remove_features(feature)
-            temp_node.set_score(dummy_evaluation(temp_node.get_features()))
+            temp_node.set_score(leave_one_out_cross_validation(data, temp_node.get_features()))
 
             current_tree_level.append(temp_node)
             temp_node.print_node()
@@ -88,7 +91,6 @@ def BE(num_features):
         else:
             return best_features
         
-        #if the best performing subset, the local max, outperforms the threshold, then we update the global max and continue searching, otherwise break and return
         threshold = 0.95 * best_features.get_score()
         local_max = current_tree_level[len(current_tree_level) - 1]
 
@@ -98,55 +100,53 @@ def BE(num_features):
         else:
             print(f"\n(Warning, Accuracy has decreased!)\n")
             return best_features
-    #exhausted all feature subsets to search
     return best_features
 
-def GFS(num_features):
+def GFS(data):
     individual_scores = []
+    max_feature = data.shape[1]
 
-    #loop through every feature, creating node, assigning score, and appending to array [individual features]
-    for i in range(1, num_features+1):
-        individual_scores.append(Node(i, dummy_evaluation(i)))
-    individual_scores = sorted(individual_scores, key=lambda node: node.get_score())
+    for i in range(1, max_feature):
+        temp_node = Node(i, leave_one_out_cross_validation(data, [i]))
+        temp_node.print_node()
+        individual_scores.append(temp_node)
     
-    #generate random evaluation for using 'zero features' i.e. the default rate
-    zero_features = dummy_evaluation(0)
-    print(f"\nUsing no features and 'random' evaluation, I get an accuarcy of {zero_features*100}%\n")
+    individual_scores = sorted(individual_scores, key=lambda node: node.get_score())
+
+    #zero_features = leave_one_out_cross_validation(data, [])
+    #print(f"\nUsing no features and 'random' evaluation, I get an accuracy of {zero_features*100}%\n")
 
     print(f"Beginning search.\n")
-    for i in range(0, num_features):
+    for i in range(len(individual_scores)):
         individual_scores[i].print_node()
-    best_features = individual_scores[num_features-1].copy_node()
+    
+    best_features = individual_scores[len(individual_scores)-1].copy_node()
 
-    if best_features.get_score() < zero_features:
-        print(f"\n(Warning, Accuracy has decreased!)\n")
-        return Node(0, zero_features)
+    #if best_features.get_score() < zero_features:
+    #    print(f"\n(Warning, Accuracy has decreased!)\n")
+    #    return Node(0, zero_features)
 
-    for _ in range(0, num_features):
-        #print the performance of the last subset
-        print(f"Feature set: {best_features.get_features()} was best, accuracy is {best_features.get_score()*100}%\n")
+    for _ in range(1, max_feature):
+        print(f"Feature set: {best_features.get_features()} was best, accuracy is {(best_features.get_score()*100):.2f}%\n")
         current_tree_level = []
 
-        #for each feature '_' create a subset of all other features except self, append each node to the array representing the level of this 'tree'
-        for feature in range(0, num_features):
+        for feature in range(1, max_feature):
             temp = best_features.get_features()
 
             if feature not in temp:
                 temp_node = best_features.copy_node()
                 temp_node.add_features(feature)
-                temp_node.set_score(dummy_evaluation(temp_node.get_features()))
+                temp_node.set_score(leave_one_out_cross_validation(data, temp_node.get_features()))
 
                 current_tree_level.append(temp_node)
                 temp_node.print_node()
 
-        #sort tree to acces best performing subset
         length = len(current_tree_level)
         if length != 0:
             current_tree_level = sorted(current_tree_level, key=lambda node: node.get_score())
         else:
             return best_features
         
-        #if the best performing subset, the local max, outperforms the global max, then we update the global max and continue searching, otherwise break and return
         local_max = current_tree_level[length-1]
         if local_max.get_score() > best_features.get_score():
             best_features = local_max
@@ -155,23 +155,78 @@ def GFS(num_features):
             return best_features
     return best_features
 
+def euclidean_distance(p1, p2):
+    return math.dist(p1, p2)
+
+class NNClassifier:
+    trainingData = []
+    trainingLabels = []
+
+    def Train(self, data):
+        self.trainingData = np.delete(data, 0, 1)
+        self.trainingLabels = data[:,0]
+
+    
+    def Test(self, instance):
+        classification = instance[0]
+        instance = np.delete(instance, 0)
+        minDist = math.inf
+        minPoint = 0
+        for idx, point in enumerate(self.trainingData):
+            tempDist = euclidean_distance(instance, point)
+            if tempDist < minDist:
+                minDist = tempDist
+                minPoint = idx
+        return self.trainingLabels[minPoint]
+
+class LOOValidator:
+    def __init__(self, subset, classifier, data):
+        self.subset = [0] + subset
+        self.classifier = classifier
+        self.data = data
+    
+    def validate(self):
+        total = 0
+        correct = 0
+        for i in range(self.data.shape[0]):
+            total += 1
+            tempClassifier = self.classifier()
+            tempClassifier.Train(np.delete(self.data[:,self.subset], i, 0))
+            if tempClassifier.Test(self.data[i,self.subset]) == self.data[i,0]:
+                correct += 1
+        return correct / total
+
+
+def leave_one_out_cross_validation(data, feature_set):
+    return LOOValidator(feature_set, NNClassifier, data).validate()
+
 def main():
     print(f"Feature Search Algorithms\n")
     print(f"\t1. 'Greedy' Forward Selection")
-    print(f"\t2. Backwards Elimination\n")
+    print(f"\t2. Backwards Elimination")
+    #print(f"\t3. Validate Feature Subset")
     
     user_selection = int(input("Enter the #(number) of which algorithm you'd like to run: "))
-    features = int(input("Enter the #(number) of features you would like to search over: "))
 
+    data_file = input("Type in the name of the file to test: ")
+    data_path = os.path.join(os.getcwd(), data_file)
+    data = np.genfromtxt(data_path)
+    
     if user_selection == 1:
-        best_subset = GFS(features)
+        best_subset = GFS(data)
         if best_subset:
             print(f"Finished search!! The best feature subset is {best_subset.get_features()}, which had an accuracy of {best_subset.get_score()*100}%\n")
 
     if user_selection == 2:
-        best_subset = BE(features)
+        best_subset = BE(data)
         if best_subset: 
             print(f"Finished search!! The best feature subset is {best_subset.get_features()}, which had an accuracy of {best_subset.get_score()*100}%\n")
+
+    #if user_selection == 3:
+        
+        #request feature subset
+        #accuracy = LOOValidator([3, 5], NNClassifier, data).validate()
+        #print(f"Accuracy with all features: {(accuracy*100):.2f}%")
 
 if __name__ == "__main__":
     main()
